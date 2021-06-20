5ed905a4298410abd48429f47c0df9af8c23f65d
Rendani <rendaniluvhengo89@gmail.com>
2021-06-20 22:50:49 +0200

    Added state for test and startTest function

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,48 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { tests } from "./data";
 
+const initialTestList = tests.map((ts) => ({
+  ...ts,
+  status: "NOT_STARTED",
+}));
+
 function Main() {
-    const [started, setStarted] =useState('Not Started')
-const [testPassed,setTestPassed= useState(0)
+  const [testList, setStateList] = useState(initialTestList);
+
+  function startTest() {
+    testList.forEach((test, index) => {
+      test.run(() => {
+        // this is called when test pass
+        console.log(index + " has passed");
+
+        // 1. copy the list
+        const testListNew = testList;
+
+        // 2. build the new test
+        testListNew[index] = {
+          ...testList[index],
+          status: "PASSED",
+        };
+
+        // 3. Update the state
+        setStateList([...testListNew]);
+      });
+
+      // Changes status to running to all of the state
+      // 1. Copy the list to a new one
+      const testListNew = testList;
+      // 2. Update the test with the new status using the index
+      testListNew[index] = {
+        ...testList[index], // the old test
+        status: "RUNNING", // new status
+      };
+      // 3. Update the state with the new list
+      setStateList([...testListNew]);
+    });
+  }
 
   return (
-    <div>
+    <div className="container">
       <table>
         <thead>
           <tr>
@@ -15,20 +51,33 @@ const [testPassed,setTestPassed= useState(0)
           </tr>
         </thead>
         <tbody>
-          {tests.map((test, key) => {
+          {testList.map((test, key) => {
             return (
               <tr>
                 <td>{test.description}</td>
-                <td>{started}</td>
+                <td class={`test-${test.status}`}>{test.status}</td>
               </tr>
             );
           })}
         </tbody>
       </table>
-      <div>
-          <ul><li>Number of test running: {testPassed} </li><li>Number of test passed: {testPassed} </li>
-          <li>Number of test failed: {testPassed} </li></ul>
-      </div>
+      <div className="test-result">
+        <ul>
+          <li>
+            Number of test running:{" "}
+            {testList.filter((ts) => ts.status == "RUNNING").length}{" "}
+          </li>
+          <li>
+            Number of test passed:{" "}
+            {testList.filter((ts) => ts.status == "PASSED").length}{" "}
+          </li>
+          <li>
+            Number of test failed:{" "}
+            {testList.filter((ts) => ts.status == "FAILED").length}{" "}
+          </li>
+        </ul>
+      </div>{" "}
+      <button onClick={startTest}>Run test</button>
     </div>
   );
 }
